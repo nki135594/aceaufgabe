ce82095b4b684f4c88c7ce7cb4d68b343e6b039c
Nao Kinoshita <bkinon@bbcag.ch>
2023-09-29 08:15:32 +0200

    commit

---
+++ ace-aufgabe/src/main/java/com/example/aceaufgabe/AceAufgabeApplication.java
@@ -0,0 +1,13 @@
+package com.example.aceaufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceAufgabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceAufgabeApplication.class, args);
+	}
+
+}
---
+++ ace-aufgabe/src/test/java/com/example/aceaufgabe/AceAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aceaufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceAufgabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
